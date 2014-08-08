47a03abc6ad5910325b8f40fb73297f6aae2e01b
eckama11 <eckhardt.aman@uwlax.edu>
2014-08-07 20:52:06 -0500

    menu

--- page.php
+++ page.php
@@ -54,15 +54,14 @@ function showError(message) {
 				</button>
 				<div class="collapse navbar-collapse navHeaderCollapse">
 					<ul class="nav navbar-nav navbar-right">
-						<li class="dropdown">
-          					<a href="#" class="dropdown-toggle" data-toggle="dropdown"> Readings </a>
-          					
-          					<ul class="dropdown-menu" role="menu">
-            					<li><a href="page.php/Data">Temperature</a></li>
-            					<li><a href="#">Another action</a></li>
-								<li><a href="#">Something else here</a></li>
-          					</ul>
-        				</li>
+						<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+							Dropdown
+							<span class="caret"></span>
+						</button>
+						<ul class="dropdown-menu" role="menu">
+						  <li><a href="page.php/Data">Temperature</a></li>
+						  <li><a href="#">Dropdown link</a></li>
+						</ul>
 						<li><a href="page.php/Pass">Password Reset</a></li>
 						<li><a href="logout.php">Logout</a></li>
 					</ul>
