ce3d346fc4fc8ebad2cbc792c2e0e114d0099729
eckama11 <eckhardt.aman@uwlax.edu>
2014-09-12 23:08:34 -0500

    asdf

--- Data.php
+++ Data.php
@@ -38,7 +38,7 @@ $js_array = $db->readTemp();
 			</script>
     	</head>
 		<body>
-    		<input type="text" style="width: 30%; margin: auto;" id="datepicker" data-provide="datepicker" placeholder="Click to choose a date..." />
+    		<input type="text" class="col-lg-1 center" id="datepicker" data-provide="datepicker" placeholder="Click to choose a date..." />
 			<br />
 			<div id="chart_div" style="width: 90%; height: 500px; margin: auto; border: 2px black solid;"></div>
 		</body>
