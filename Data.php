52ccd9aef4a07ece4c8b6d36ef27001cfb53c10b
eckama11 <eckhardt.aman@uwlax.edu>
2014-09-12 22:22:32 -0500

    asdf

--- Data.php
+++ Data.php
@@ -73,10 +73,11 @@ $js_array = $db->readTemp();
 			?>
 			</table>
 			-->
-			<div class="input-append date" id="dp3" data-date="12-02-2012" data-date-format="dd-mm-yyyy">
+			<!--<div class="input-append date" id="dp3" data-date="12-02-2012" data-date-format="dd-mm-yyyy">
            		<input class="span2" size="16" type="text" value="12-02-2012">
            		<span class="add-on"><i class="icon-th"></i></span>
 			</div>
+			-->
 			<br />
 			<div id="chart_div" style="width: 90%; height: 500px; margin: auto; border: 2px black solid;"></div>
 		</body>
