e32ea46f248c2c179229bdba2201b04db1167e1f
eckama11 <eckhardt.aman@uwlax.edu>
2014-09-12 21:13:31 -0500

    random

--- Data.php
+++ Data.php
@@ -64,14 +64,14 @@ $js_array = $db->readTemp();
 				<th>Particles (ppm)</th>
 			</tr>
 			<?php
-				foreach($sensor as $reading){
+				/*foreach($sensor as $reading){
 					$values = $reading->toArray();
 					echo "<tr>";
 					foreach($values as $key=>$val){
 						echo "<td>$val</td>";
 					}
 					echo "</tr>";
-				}
+				}*/
 			?>
 			</table>
 			-->
@@ -79,6 +79,7 @@ $js_array = $db->readTemp();
             <div>
             	<input data-provide="datepicker" data-date-autoclose="true" data-date-today-btn="true" data-date-today-highlight="true" class="form-control" type="text" name="startDate" id="startDate" placeholder="Enter employment start date" />
 			</div>
+			<div></div>
 			<div id="chart_div" style="width: 90%; height: 500px; margin: auto; border: 2px black solid;"></div>
 		</body>
 	</html>
