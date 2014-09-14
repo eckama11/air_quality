ae03e5b95b249a7c749b1757cbc6f741b6389889
eckama11 <eckhardt.aman@uwlax.edu>
2014-09-14 15:44:03 -0500

    change time to check

--- page.php
+++ page.php
@@ -9,7 +9,7 @@ if (!isset($loginSession))
 else if ((substr($page, 0, strlen($prefix)) != $prefix) || !is_readable($page))
     doUnauthorizedRedirect();
     
-if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 10)) {
     // last request was more than 30 minutes ago
     session_unset();     // unset $_SESSION variable for the run-time 
     session_destroy();   // destroy session data in storage
