16cb753f872a999b5880a2e0e87c02784f023699
eckama11 <eckhardt.aman@uwlax.edu>
2014-12-02 01:20:08 -0600

    was needed

--- classes/DBInterface.php
+++ classes/DBInterface.php
@@ -109,6 +109,10 @@ class DBInterface {
 							")"
 					);
 				
+				$success = $loginStmt->execute(Array(':username' => $username));
+				if (!$success)
+					throw new Exception($this->formatErrorMessage($loginStmt, "Unable to query database to authenticate User"));
+
 				$row = $loginStmt->fetchObject();
 				if (!$row)
 					throw new Exception("Unable to authenticate User, incorrect username or password");
@@ -136,7 +140,7 @@ class DBInterface {
     		throw new Exception($this->formatErrorMessage($loginStmt, "Unable to query database to authenticate Username: Internal Error"));
     	} 
         return $rv;
-        
+        //Request failed, unable to login: Int
 
     } // createLoginSession
 
