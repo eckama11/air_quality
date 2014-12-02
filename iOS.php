0acd6ed2a2eca142ae032e80d3dc00312a2bd05e
eckama11 <eckhardt.aman@uwlax.edu>
2014-12-02 01:52:40 -0600

    using escaped strings

--- iOS.php
+++ iOS.php
@@ -3,11 +3,6 @@
 require_once("creds.php");
 $conn = new mysqli("localhost", $dbUsername, $dbPassword, $dbName);
 
-$username = "eckama";//$_POST['username'];
-$password = "password";//$_POST['password'];
-
-$query = "SELECT password FROM user WHERE username = '$username'";
-
 if (!$conn) 
 {
 	die('Connect Error ' . $conn->errno . ': ' . $conn->error);    
@@ -15,6 +10,11 @@ if (!$conn)
 
 else 
 {
+	$username = $conn->real_escape_string($_POST['username']);
+	$password = $conn->real_escape_string($_POST['password']);
+
+	$query = "SELECT password FROM user WHERE username = '$username'";
+	
 	$result = $conn->query($query)->fetch_assoc();
     if(password_verify($password, $result['password'] ) )
     {
