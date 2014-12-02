8f84a41edc319ec20251ef0ec3979075bbaa0420
eckama11 <eckhardt.aman@uwlax.edu>
2014-12-02 00:21:37 -0600

    wefasdf

--- page.php
+++ page.php
@@ -73,9 +73,9 @@ else if ((substr($page, 0, strlen($prefix)) != $prefix) || !is_readable($page))
 		</div>
 
         <div id="messageAlert" class="alert alert-danger" style="display:none;position:fixed;left:20px;right:20px">
-            <span id="message"></span>
+            <span id="message"><h1>FUCK THIS SHIT</h1></span>
         </div>
-<?php	echo "<h1>FUCK THIS SHIT</h1>"; require_once($page); ?>
+<?php require_once($page); ?>
 		<!-- JavaScript -->
 		<script type="text/javascript" src="js/jquery1_11_1.js"></script>
 		<script type="text/javascript" src="js/bootstrap.js"></script>
