b667cae1d454d90311706f6b7ac5a81c2bfc47cf
eckama11 <eckhardt.aman@uwlax.edu>
2014-12-08 15:58:04 -0600

    g

--- iOS.php
+++ iOS.php
@@ -3,55 +3,26 @@
 require_once("creds.php");
 $conn = new mysqli("localhost", $dbUsername, $dbPassword, $dbName);
 
-/* check connection */
 if (!$conn) 
 {
-	echo '{"success":0,"error_message":"' . mysqli_connect_error() . '"}';
 	die('Connect Error ' . $conn->errno . ': ' . $conn->error);    
 } 
 
 else 
 {
-	$username = "eckama";//$conn->real_escape_string($_POST['username']);
-	$password = "password";//$conn->real_escape_string($_POST['password']);
+	$username = $conn->real_escape_string($_POST['username']);
+	$password = $conn->real_escape_string($_POST['password']);
 
 	$query = "SELECT password FROM user WHERE username = '$username'";
 	
 	$result = $conn->query($query)->fetch_assoc();
     if(password_verify($password, $result['password'] ) )
     {
-    	if ($stmt = $mysqli->prepare("SELECT deviceId FROM user WHERE username = ?")) 
-    	{
-			/* bind parameters for markers */
-			$stmt->bind_param("ss", $username);
-
-			/* execute query */
-			$stmt->execute();
-
-			/* bind result variables */
-			$stmt->bind_result($deviceId);
-
-			/* fetch value */
-			$stmt->fetch();
-
-			/* close statement */
-			$stmt->close();
-		}
-
-		/* close connection */
-		$mysqli->close();
-		
-		if ($deviceId) 
-		{
-			echo '{"success":1}';
-		} 
-		else 
-		{
-			echo '{"success":0,"error_message":"Invalid Username/Password"}';
-		}
+    	echo "true";
     }
     else
     {
-    	echo '{"success":0,"error_message":"Username and password do not match."}';
+    	echo "False";
     }
 }
+//aslkdjf
