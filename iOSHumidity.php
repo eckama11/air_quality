49445cf720d9a670a68deb2873a525edc93ebf99
eckama11 <eckhardt.aman@uwlax.edu>
2014-12-08 20:01:30 -0600

    changes for checking

--- iOSHumidity.php
+++ iOSHumidity.php
@@ -11,25 +11,17 @@ if (!$conn)
 else 
 {
 	
-    	$deviceQuery = "SELECT deviceId FROM user WHERE username = '$username'";
-    	$resultDQ = $conn->query($deviceQuery)->fetch_assoc();
-    	$deviceId= $resultDQ['deviceId'];
-    	$sql = "SELECT DATE_FORMAT(timeInfo,'%r') as 'timeInfo', pressure FROM sensors WHERE DATE(timeInfo) = DATE(NOW()) ";
-            "AND impId='$deviceId'";
-    	$humQuery = "Select DATE_FORMAT(timeInfo,'%r') as 'timeInfo', humidity FROM sensors WHERE DATE(timeInfo) = AND impId = '$deviceId'";
-    	//$rv[] = array('Time', 'Pressure');
-    	/* while ($row = $stmt->fetchObject()) {
-            $rv[] = array(
-            		$row->timeInfo,
-					(double)$row->pressure
-                );
-        } // while
-        */
-        
-    	echo '{"success":1, "hum":"'.$humQuery.'"}';
-    }
-    else
-    {
-    	echo '{"success":0,"error_message":"Humidity Temps not available"}';
+	$deviceId = "2336c2eb6e4936ee";
+	$humQuery = "Select DATE_FORMAT(timeInfo,'%r') as 'timeInfo', humidity FROM sensors WHERE DATE(timeInfo) = DATE(timeInfo) = DATE(NOW()) AND impId = '$deviceId'";
+	//$rv[] = array('Time', 'Pressure');
+	/* while ($row = $stmt->fetchObject()) {
+		$rv[] = array(
+				$row->timeInfo,
+				(double)$row->pressure
+			);
+	} // while
+	*/
+	
+	echo '{"success":1, "hum":"'.$humQuery.'"}';
     }
 }
