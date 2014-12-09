9efc1bcce640213599582099a2ded0f1b25ab3ce
eckama11 <eckhardt.aman@uwlax.edu>
2014-12-08 20:34:53 -0600

    :) mew

--- iOSHumidity.php
+++ iOSHumidity.php
@@ -13,7 +13,7 @@ else
 	
 	
 	$deviceId = "2336c2eb6e4936ee";
-	if ($result = $mysqli->query("Select DATE_FORMAT(timeInfo,'%r') as 'timeInfo', humidity FROM sensors WHERE DATE(timeInfo) = DATE(timeInfo) = DATE(NOW()) AND impId = '$deviceId'")
+	if ($result = $mysqli->query("SELECT DATE_FORMAT(timeInfo,'%r') as 'timeInfo', humidity FROM sensors WHERE DATE(timeInfo) = DATE(NOW()) AND impId='$deviceId'")
 	{
 		echo "hello new world :)";
 	/*
