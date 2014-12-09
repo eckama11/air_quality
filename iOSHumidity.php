eb760e53e648e6f086542355a74f9213fbfab2fb
eckama11 <eckhardt.aman@uwlax.edu>
2014-12-08 20:59:27 -0600

    datee

--- iOSHumidity.php
+++ iOSHumidity.php
@@ -11,7 +11,7 @@ if (!$mysqli)
 else 
 {
 	$deviceId = "2336c2eb6e4936ee";
-	$date = date('%r');//$_POST['date'];
+	$date = date('r');//$_POST['date'];
 	echo $date;
 	if ($result = $mysqli->query("SELECT DATE_FORMAT(timeInfo,'%r') as 'timeInfo', humidity FROM sensors WHERE DATE(timeInfo) = '$date'".
             "AND impId='$deviceId'"))
