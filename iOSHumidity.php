59502cb5d6b5b28dff6ec848283b4fb8e9a74fe0
eckama11 <eckhardt.aman@uwlax.edu>
2014-12-08 20:55:31 -0600

    d

--- iOSHumidity.php
+++ iOSHumidity.php
@@ -11,25 +11,15 @@ if (!$mysqli)
 else 
 {
 	$deviceId = "2336c2eb6e4936ee";
-	if ($result = $mysqli->query("SELECT DATE_FORMAT(timeInfo,'%r') as 'timeInfo', humidity FROM sensors WHERE DATE(timeInfo) = DATE(NOW()) ".
+	$date = DATE(NOW());
+	if ($result = $mysqli->query("SELECT DATE_FORMAT(timeInfo,'%r') as 'timeInfo', humidity FROM sensors WHERE DATE(timeInfo) = '$date'".
             "AND impId='$deviceId'"))
-    {
-	/*
-		//$rv[] = array('Time', 'Pressure');
-		 while ($row = $stmt->fetchObject()) {
-			$rv[] = array(
-					$row->timeInfo,
-					(double)$row->pressure
-				);
-		} // while
-		
-	*/	
+    {	
 		$inc = 1;
 		while($resultSet = $result->fetch_assoc()){
 			echo "Result $inc: {$resultSet['timeInfo']} - {$resultSet['humidity']}<br/>\n";
 			$inc++;
 		}
-		$resultSet->close();
 		$result->close();
 	
     }
