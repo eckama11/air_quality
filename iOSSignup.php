a06020f50a58bea61066207c5b18fd8a38364e1d
eckama11 <eckhardt.aman@uwlax.edu>
2014-12-14 17:51:26 -0600

    change in signup query

--- iOSSignup.php
+++ iOSSignup.php
@@ -12,20 +12,28 @@ else
 {
 	$username = $conn->real_escape_string($_POST['username']);
 	$password = $conn->real_escape_string($_POST['password']);
+	$email = $conn->real_escape_string($_POST['email']);
+	$deviceId = $conn->real_escape_string($_POST['device']);
 
-	$query = "SELECT password FROM user WHERE username = '$username'";
+	$query = "SELECT id FROM user WHERE username = '$username'";
 	
-	$result = $conn->query($query)->fetch_assoc();
+	if ($username == "" || $password == "" || $email == "" || $deviceId == "") {
+		echo '{"success":0,"error_message":"All fields are required."}';
+	}
 	
-    if(password_verify($password, $result['password'] ) )
-    {
-    	$deviceQuery = "SELECT deviceId FROM user WHERE username = '$username'";
-    	$resultDQ = $conn->query($deviceQuery)->fetch_assoc();
-    	$deviceId= $resultDQ['deviceId'];
-    	echo '{"success":1, "hum":"'.$deviceId.'"}';
-    }
+	/* Select queries return a resultset */
+	if ($result = $mysqli->query($query)) {
+
+		/* free result set */
+		$result->close();
+		echo '{"success":0,"error_message":"Username already in use."}';
+	}
     else
     {
-    	echo '{"success":0,"error_message":"Invalid Username/Password"}';
+    
+    	$addUserQuery = "INSERT INTO user username, password, email, deviceId VALUES '$username', '$password', '$email', '$deviceId'";
+    	$mysqli->query($addUserQuery);
+    	$mysqli->close();
+    	echo '{"success":1, "hum":"'.$deviceId.'"}';
     }
 }
