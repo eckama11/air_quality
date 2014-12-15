7947e31ff2e69d6456b5479702de7356ed977b63
eckama11 <eckhardt.aman@uwlax.edu>
2014-12-14 18:55:39 -0600

    simply

--- iOSSignup.php
+++ iOSSignup.php
@@ -1,26 +1,23 @@
 <?php
 require_once("common.php");
 
+$username = @$_POST['username'];
+$email = @$_POST['email'];
+$deviceId = @$_POST['device'];
+$password1 = @$_POST['password'];
 
-if($_POST) {
-	$username = @$_POST['username'];
-	$email = @$_POST['email'];
-	$deviceId = @$_POST['device'];
-	$password1 = @$_POST['password'];
-
-	try {
-		if ($db->isUsernameInUse($username)) {
-			echo '{"success":0,"error_message":"Username Exist."}';
-		}
-		else {
-			$id = 0;
-			$passwordHash = password_hash($password1, PASSWORD_DEFAULT);
-			$user1 = new User($id, $username, $passwordHash, $email, $deviceId); 
-			$user = $db->writeUser($user1);
-			echo '{"success":1}';
-		}
+try {
+	if ($db->isUsernameInUse($username)) {
+		echo '{"success":0,"error_message":"Username Exist."}';
 	}
-	catch (Exception $ex) {
-    	echo '{"success":0,"error_message":"Error happened."}';
+	else {
+		$id = 0;
+		$passwordHash = password_hash($password1, PASSWORD_DEFAULT);
+		$user1 = new User($id, $username, $passwordHash, $email, $deviceId); 
+		$user = $db->writeUser($user1);
+		echo '{"success":1}';
 	}
+}
+catch (Exception $ex) {
+	echo '{"success":0,"error_message":"Error happened."}';
 }
