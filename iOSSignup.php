71719d0e21bfe638d12bbb8a68a145567c76ac60
eckama11 <eckhardt.aman@uwlax.edu>
2014-12-14 18:53:40 -0600

    changes

--- iOSSignup.php
+++ iOSSignup.php
@@ -1,6 +1,4 @@
 <?php
-header('Content-type: application/json');
-require_once("creds.php");
 require_once("common.php");
 
 
@@ -10,25 +8,19 @@ if($_POST) {
 	$deviceId = @$_POST['device'];
 	$password1 = @$_POST['password'];
 
-	$mysqli = new mysqli("localhost", $dbUsername, $dbPassword, $dbName);
-	/* check connection */
-	if (mysqli_connect_errno()) {
-	error_log("Connect failed: " . mysqli_connect_error());
-		echo '{"success":0,"error_message":"' . mysqli_connect_error() . '"}';
-	} else {
-		try {
-			if ($db->isUsernameInUse($username)) {
-				echo '{"success":0,"error_message":"Username Exist."}';
-			}
-			else {
-				$id = 0;
-				$passwordHash = password_hash($password1, PASSWORD_DEFAULT);
-				$user1 = new User($id, $username, $passwordHash, $email, $deviceId); 
-				$user = $db->writeUser($user1);
-				echo '{"success":1}';
-			}
+	try {
+		if ($db->isUsernameInUse($username)) {
+			echo '{"success":0,"error_message":"Username Exist."}';
 		}
-	} catch (Exception $ex) {
+		else {
+			$id = 0;
+			$passwordHash = password_hash($password1, PASSWORD_DEFAULT);
+			$user1 = new User($id, $username, $passwordHash, $email, $deviceId); 
+			$user = $db->writeUser($user1);
+			echo '{"success":1}';
+		}
+	}
+	catch (Exception $ex) {
     	echo '{"success":0,"error_message":"Error happened."}';
 	}
 }
