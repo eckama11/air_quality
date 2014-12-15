6a40df4246c5d97b91c6ccfc7b7cce8e6b62632b
eckama11 <eckhardt.aman@uwlax.edu>
2014-12-14 21:43:10 -0600

    N

--- iOSHumidity.php
+++ iOSHumidity.php
@@ -22,8 +22,9 @@ else
 		while($resultSet = $result->fetch_assoc()){
 			$time[$inc] = $resultSet['timeInfo'];
 			$humidity[$inc] = $resultSet['humidity'];
-			echo '{"time: ". $time[$inc]}';
-			echo '{"humidity: ". $humidity[$inc]}';
+			echo '{"success":1, "hum":"'.$deviceId.'"}'
+			echo '{"time:"'.$time[$inc].'"}';
+			echo '{"humidity:"'.$humidity[$inc].'"}';
 			//echo "Result $inc: {$resultSet['timeInfo']} - {$resultSet['humidity']}<br/>\n";
 			$inc++;
 		}
