11a95ddad5a83fc99b667f4b9fc707be0635fb26
eckama11 <eckhardt.aman@uwlax.edu>
2015-02-16 09:28:52 -0600

    fix pressure dbinterface

--- classes/DBInterface.php
+++ classes/DBInterface.php
@@ -548,11 +548,11 @@ class DBInterface {
     */
     // THIS WILL NEED TO TAKE IN A SENSOR or USER TO OUTPUT JUST ONE SENSOR'S DATA or USER'S DATA
     // WILL NEED TO TAKE IN DATE TO SHOW now it shows current date
-    public function readPressure( $device ) {
+    public function readPressure($date, $device ) {
         static $stmt;
         $deviceId = $device;
         if ($stmt == null)
-        	$sql = "SELECT DATE_FORMAT(timeInfo,'%r') as 'timeInfo', pressure FROM sensors WHERE DATE(timeInfo) = DATE(NOW()) ";
+        	$sql = "SELECT DATE_FORMAT(timeInfo,'%r') as 'timeInfo', pressure FROM sensors WHERE DATE_FORMAT(timeInfo,'%m/%d/%Y') = '$date' ";
             "AND impId='$deviceId'";
             $stmt = $this->dbh->prepare($sql);
              
