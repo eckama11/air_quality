424b91c8ca1b2b2e61cfead83f8fe2dc5082c16a
eckama11 <eckhardt.aman@uwlax.edu>
2015-02-15 23:11:31 -0600

    test

--- Humidity.php
+++ Humidity.php
@@ -6,9 +6,9 @@ require_once("common.php");
 //$sensor = $db->readSensors();
 $device = $loginSession->authenticatedUser->deviceId;
 if(isset($_POST['calDate'])){
-	$date = date('Y-m-d',$_POST['calDate']); //Use posted date
+	$date = date('m\d\Y',$_POST['calDate']); //Use posted date
 }else{
-	$date = date('Y-m-d'); //Default to today
+	$date = date('m\d\Y'); //Default to today
 }
 $js_array = $db->readHumidity($date,$device);
 $readingCount = count($js_array);
@@ -43,7 +43,7 @@ $readingCount = count($js_array);
   			<script type="text/javascript">
 				// Init date picker and display UI
 				$('#datepicker').datepicker({
-					format: "yyyy-mm-dd",
+					format: "mm\dd\yyyy",
 					clearBtn: true,
 					todayBtn: "linked",
 					autoclose: true,
