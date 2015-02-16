2cbdffed28aaeddcb7f2d4fe982197bd3c6c3d6b
eckama11 <eckhardt.aman@uwlax.edu>
2015-02-16 00:05:10 -0600

    fix

--- Particles.php
+++ Particles.php
@@ -14,60 +14,39 @@ if(isset($_POST['calDate'])){
 $js_array = $db->readParticles($date,$device);
 $readingCount = count($js_array);
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-	<html xmlns="http://www.w3.org/1999/xhtml">
-		<head>
-			<base href="<?php echo htmlentities(BASE_URL); ?>">
-			<title>Readings</title>
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<!-- StyleSheet -->
-    		<script type="text/javascript">
-				google.load("visualization", "1", {packages:["corechart"]});
-				google.setOnLoadCallback(drawChart);
-				function drawChart() {
-					var data = google.visualization.arrayToDataTable(<?= json_encode($js_array); ?>);
 
-					var options = {
-					  title: 'Particle Readings'
-					};
+<script type="text/javascript">
+	window.onresize = resize;
 
-					var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-					chart.draw(data, options);
-				  }
-    		</script>
-    		<script type="text/javascript">
-				window.onresize = resize;
-
-				function resize()
-				{
-					window.location.reload();
-				}
-			</script>
-    		<script type="text/javascript">
-				// Init date picker and display UI
-				$('#datepicker').datepicker();
-			</script>
-    	</head>
-		<body>
-		<form method="post">
-    		<input name='calDate' value="<?php echo $date; ?>" type="text" class="form-control" id="datepicker" style="display: block; text-align:center; width: 20%; margin-left: auto; margin-right: auto; border: 2px black solid;" data-provide="datepicker" placeholder="Click to choose a date..." />
-  			<script type="text/javascript">
-				// Init date picker and display UI
-				$('#datepicker').datepicker({
-					format: "mm/dd/yyyy",
-					clearBtn: true,
-					todayBtn: "linked",
-					autoclose: true,
-					endDate: "<?php echo $today;?>",
-					todayHighlight: true
-				}).on('changeDate', function(){
-					$(this).parent('form').submit();
-				});
-			</script>
-		</form>
-    		<br />
-			<?php if($readingCount > 1) : ?>
-			<script type="text/javascript">
+	function resize()
+	{
+		window.location.reload();
+	}
+</script>
+<script type="text/javascript">
+	// Init date picker and display UI
+	$('#datepicker').datepicker();
+</script>
+<body>
+	<form method="post">
+		<input name='calDate' value="<?php echo $date; ?>" type="text" class="form-control" id="datepicker" style="display: block; text-align:center; width: 20%; margin-left: auto; margin-right: auto; border: 2px black solid;" data-provide="datepicker" placeholder="Click to choose a date..." />
+		<script type="text/javascript">
+			// Init date picker and display UI
+			$('#datepicker').datepicker({
+				format: "mm/dd/yyyy",
+				clearBtn: true,
+				todayBtn: "linked",
+				autoclose: true,
+				endDate: "<?php echo $today;?>",
+				todayHighlight: true
+			}).on('changeDate', function(){
+				$(this).parent('form').submit();
+			});
+		</script>
+	</form>
+	<br />
+	<?php if($readingCount > 1) : ?>
+	<script type="text/javascript">
 		google.load("visualization", "1", {packages:["corechart"]});
 		google.setOnLoadCallback(drawChart);
 		function drawChart() {
@@ -79,10 +58,10 @@ $readingCount = count($js_array);
 			var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
 			chart.draw(data, options);
 		}
-    </script>
-    			<div id="chart_div" style="width: 90%; height: 500px; margin: auto; border: 2px black solid;"></div>
-			<?php else : ?>
-				<div class="alert alert-danger" role="alert" style="width: 50%; margin: auto;">There is no data for this day :(</div>
-			<?php endif; ?>
-		</body>
+	</script>
+		<div id="chart_div" style="width: 90%; height: 500px; margin: auto; border: 2px black solid;"></div>
+	<?php else : ?>
+		<div class="alert alert-danger" role="alert" style="width: 50%; margin: auto;">There is no data for this day :(</div>
+	<?php endif; ?>
+</body>
 	</html>
