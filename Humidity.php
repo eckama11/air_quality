f76a5f12450bd9cd5abeb2bba0a8d0bc2ac18940
eckama11 <eckhardt.aman@uwlax.edu>
2015-02-15 23:13:23 -0600

    asdfasdf

--- Humidity.php
+++ Humidity.php
@@ -6,9 +6,9 @@ require_once("common.php");
 //$sensor = $db->readSensors();
 $device = $loginSession->authenticatedUser->deviceId;
 if(isset($_POST['calDate'])){
-	$date = date('m\\d\\Y',$_POST['calDate']); //Use posted date
+	$date = date('m\d\Y',$_POST['calDate']); //Use posted date
 }else{
-	$date = date('m\\d\\Y'); //Default to today
+	$date = date('m\d\Y'); //Default to today
 }
 $js_array = $db->readHumidity($date,$device);
 $readingCount = count($js_array);
