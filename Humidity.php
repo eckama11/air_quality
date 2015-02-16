8cc6cd72da00301c2ceff48fd68399768d68ad38
eckama11 <eckhardt.aman@uwlax.edu>
2015-02-15 22:32:22 -0600

    more datepicker js

--- Humidity.php
+++ Humidity.php
@@ -41,17 +41,18 @@ $readingCount = count($js_array);
 			</script>
     		<script type="text/javascript">
 				// Init date picker and display UI
-				$('#datepicker').datepicker({
+				$('.datepicker').datepicker({
 					format: 'yyyy-mm-dd',
 					autoclose: true,
 					clearBtn: true,
-					todayBtn: true
+					todayBtn: true,
+					forceParse: false
 				});
 			</script>
     	</head>
 		<body>
 		
-    		<input type="text" class="form-control" style="display: block; text-align:center; width: 20%; margin-left: auto; margin-right: auto; border: 2px black solid;" id="datepicker" 
+    		<input type="text" class="form-control datepicker" style="display: block; text-align:center; width: 20%; margin-left: auto; margin-right: auto; border: 2px black solid;" id="datepicker" 
     		data-provide="datepicker" data-date-format="yyyy-mm-dd" placeholder="Click to choose a date..." />
   			
     		<br />
