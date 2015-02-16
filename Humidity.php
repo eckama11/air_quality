8f1ab6857b2bd678bdb164991a11d62447cb36eb
eckama11 <eckhardt.aman@uwlax.edu>
2015-02-15 22:47:50 -0600

    maybe some fixes?

--- Humidity.php
+++ Humidity.php
@@ -9,51 +9,41 @@ $date = date('Y-m-d');
 $js_array = $db->readHumidity($date,$device);
 $readingCount = count($js_array);
 ?>
+    <script type="text/javascript">
+		google.load("visualization", "1", {packages:["corechart"]});
+		google.setOnLoadCallback(drawChart);
+		function drawChart() {
+			var data = google.visualization.arrayToDataTable(<?= json_encode($js_array); ?>);
+			var options = {
+				title: 'Humidity Readings'
+			};
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-	<html xmlns="http://www.w3.org/1999/xhtml">
-		<head>
-			<base href="<?php echo htmlentities(BASE_URL); ?>">
-			<title>Readings</title>
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<!-- StyleSheet -->
-    		<script type="text/javascript">
-				google.load("visualization", "1", {packages:["corechart"]});
-				google.setOnLoadCallback(drawChart);
-				function drawChart() {
-					var data = google.visualization.arrayToDataTable(<?= json_encode($js_array); ?>);
-
-					var options = {
-					  title: 'Humidity Readings'
-					};
-
-					var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-					chart.draw(data, options);
-				  }
-    		</script>
+			var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+			chart.draw(data, options);
+		}
+    </script>
     		<script type="text/javascript">
 				window.onresize = resize;
-
 				function resize()
 				{
 					window.location.reload();
 				}
 			</script>
-    		<script type="text/javascript">
-				// Init date picker and display UI
-				$('.datepicker').datepicker({
-					format: 'yyyy-mm-dd',
-					clearBtn: true,
-					todayBtn: true,
-					autoclose: true
-				});
-			</script>
+    		
     	</head>
 		<body>
 		
     		<input type="text" class="form-control datepicker" style="display: block; text-align:center; width: 20%; margin-left: auto; margin-right: auto; border: 2px black solid;" 
     		data-provide="datepicker" data-date-format="yyyy-mm-dd" placeholder="Click to choose a date..." />
-  			
+  			<script type="text/javascript">
+				// Init date picker and display UI
+				$('.datepicker').datepicker({
+					format: "yyyy-mm-dd",
+					clearBtn: true,
+					todayBtn: "linked",
+					autoclose: true
+				});
+			</script>
     		<br />
 			<?php if($readingCount > 1) : ?>
     			<div id="chart_div" style="width: 90%; height: 500px; margin: auto; border: 2px black solid;"></div>
